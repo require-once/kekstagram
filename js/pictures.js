8a2b12cef12068344828d7ad5fab3ebbaa1b09c3
require-once <dimas.zhukov@gmail.com>
2020-06-30 16:54:50 +0300

    task4: Открытие больших фоток при клике по миниатютре, загрузка новой фотки

--- js/pictures.js
+++ js/pictures.js
@@ -18,12 +18,16 @@ var DESCRIPTIONS = [
 	'Вот это тачка!',
 ];
 
+var ENTER_KEYCODE = 13;
+var ESC_KEYCODE = 27;
+
 var getRandomInteger = function(min, max) {
   var rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 };
 
 
+/* Генерация фоток пользователей */
 var generetePhotos = function() {
 	var photos = [];
 	
@@ -51,6 +55,7 @@ var generetePhotos = function() {
 var user_photos = generetePhotos();
 
 
+/* Отрисовка фоток пользователей */
 var renderPictures = function(user_photos) {
 	var template = document.querySelector('#picture').content.querySelector('a');
 	var fragment = document.createDocumentFragment();
@@ -69,32 +74,120 @@ var renderPictures = function(user_photos) {
 renderPictures(user_photos);
 
 
-var showBigPicture = function() {
+/* Обработчики закрытия большой фотки */
+var onBigPictureCloseButtonPress = function(evt) {
+	if (evt.keyCode === ESC_KEYCODE) {
+		closeBigPicture();
+	}
+};
+
+var closeBigPicture = function() {
+	document.body.classList.remove('modal-open');
+	document.querySelector('.big-picture').classList.add('hidden');
+	document.removeEventListener('keydown', onBigPictureCloseButtonPress);
+};
+
+
+/* Показ большой фотки */
+var showBigPicture = function(index) {
+	document.body.classList.add('modal-open');
 	document.querySelector('.big-picture').classList.remove('hidden');
-	document.querySelector('.big-picture__img img').src = user_photos[0].url;
-	document.querySelector('.big-picture__img img').alt = user_photos[0].description;
-	document.querySelector('.likes-count').textContent = user_photos[0].likes;
-	document.querySelector('.comments-count').textContent = user_photos[0].comments.length;
+	document.querySelector('.big-picture__img img').src = user_photos[index].url;
+	document.querySelector('.big-picture__img img').alt = user_photos[index].description;
+	document.querySelector('.likes-count').textContent = user_photos[index].likes;
+	document.querySelector('.comments-count').textContent = user_photos[index].comments.length;
 	document.querySelector('.social__comment .social__picture').src = 'img/avatar-' + getRandomInteger(1, 6) + '.svg';
-	document.querySelector('.social__caption').textContent = user_photos[0].description;
+	document.querySelector('.social__caption').textContent = user_photos[index].description;
 	
 	var socialComment = document.querySelector('.social__comments');
 	var commentsTexts = document.querySelectorAll('.social__comment .social__text');
 	
-	if (user_photos[0].comments.length === 1) {
-		commentsTexts[0].textContent = user_photos[0].comments[0];
-		socialComment.removeChild(socialComment.children[1]);
-	} else if (user_photos[0].comments.length === 2) {
-		commentsTexts[0].textContent = user_photos[0].comments[0];
-		commentsTexts[1].textContent = user_photos[0].comments[1];
+	if (user_photos[index].comments.length === 1) {
+		commentsTexts[0].textContent = user_photos[index].comments[0];
+		socialComment.children[1].classList.add('hidden');
+	} else if (user_photos[index].comments.length === 2) {
+		socialComment.children[1].classList.remove('hidden');
+		commentsTexts[0].textContent = user_photos[index].comments[0];
+		commentsTexts[1].textContent = user_photos[index].comments[1];
 	}
+	
+	document.querySelector('.big-picture #picture-cancel').addEventListener('click', closeBigPicture);
+	document.addEventListener('keydown', onBigPictureCloseButtonPress);
 };
-
-showBigPicture();
 
 
 document.querySelector('.social__comment-count').classList.add('visually-hidden');
 document.querySelector('.social__comments-loader').classList.add('visually-hidden');
 
 
-//console.dir(USER_PHOTOS[0]);
+/* Загрузка новой фотки */
+var uploadFileInput = document.querySelector('#upload-file');
+var uploadForm = document.querySelector('.img-upload__overlay');
+var closeButton = document.querySelector('#upload-cancel');
+
+var onUploadFormCloseButtonPress = function(evt) {
+	if (evt.keyCode === ESC_KEYCODE) {
+		closeUploadForm();
+	}
+};
+
+var closeUploadForm = function() {
+	uploadForm.classList.add('hidden');
+	document.removeEventListener('keydown', onUploadFormCloseButtonPress);
+};
+
+
+uploadFileInput.addEventListener('change', function() {
+	uploadForm.classList.remove('hidden');
+	document.addEventListener('keydown', onUploadFormCloseButtonPress);
+});
+
+closeButton.addEventListener('click', function() {
+	closeUploadForm();
+}); 
+ 
+
+/* Изменение масштаба */
+var scaleSmallerButton = document.querySelector('.img-upload__overlay .scale__control--smaller');
+var scaleBiggerButton = document.querySelector('.img-upload__overlay .scale__control--bigger');
+var scaleValueInput = document.querySelector('.img-upload__overlay .scale__control--value');
+var imagePreview = document.querySelector('.img-upload__overlay .img-upload__preview');
+
+scaleBiggerButton.addEventListener('click', function() {
+	var resizeValue = parseInt(scaleValueInput.value);
+	
+	if (resizeValue < 100) {
+		scaleValueInput.value = resizeValue + 25 + '%';
+		imagePreview.style.transform = 'scale(' + (resizeValue + 25) / 100 + ')';
+	}
+});
+
+scaleSmallerButton.addEventListener('click', function() {
+	var resizeValue = parseInt(scaleValueInput.value);
+	
+	if (resizeValue > 25) {
+		scaleValueInput.value = resizeValue - 25 + '%';
+		imagePreview.style.transform = 'scale(' + (resizeValue - 25) / 100 + ')';
+	}
+});
+
+
+/* Применение фильтров для загружаемой фотки */
+var effectInput = document.querySelector('.img-upload__overlay input[name=effect]');
+
+effectInput.parentNode.parentNode.addEventListener('change', function(evt) {
+	imagePreview.className = 'img-upload__preview';
+	imagePreview.classList.add('effects__preview--' + evt.target.value);
+});
+
+
+/* Показ большой фотки по клику по миниатюре */
+var miniatures = document.querySelectorAll('.pictures.container a');
+
+miniatures.forEach(function(currentItem, index) {
+	currentItem.addEventListener('click', function() {
+		showBigPicture(index);
+	});
+});
+
+
