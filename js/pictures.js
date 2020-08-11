b7dccd3b316952313034c431680689a0fdbf1c82
require-once <dimas.zhukov@gmail.com>
2020-08-11 23:05:55 +0300

    task5: Изменение глубины фильтра при перемещении слайдера

--- js/pictures.js
+++ js/pictures.js
@@ -174,10 +174,92 @@ scaleSmallerButton.addEventListener('click', function() {
 
 /* Применение фильтров для загружаемой фотки */
 var effectInput = document.querySelector('.img-upload__overlay input[name=effect]');
+var effectLevelInput = document.querySelector('.effect-level__value');
+var wholeSlider = document.querySelector('.effect-level');
+var pin = document.querySelector('.effect-level__pin');
+var levelDepth = document.querySelector('.effect-level__depth');
 
 effectInput.parentNode.parentNode.addEventListener('change', function(evt) {
 	imagePreview.className = 'img-upload__preview';
 	imagePreview.classList.add('effects__preview--' + evt.target.value);
+	
+	if (evt.target.value === 'none') {
+		wholeSlider.classList.add('hidden');
+	} else {
+		wholeSlider.classList.remove('hidden');
+	}
+	
+	imagePreview.style.filter = '';
+	effectLevelInput.value = 100;
+	pin.style.left = '450px';
+	levelDepth.style.width = '450px';
+});
+
+
+/* Перемещение слайдера */
+wholeSlider.classList.add('hidden');
+pin.style.left = '450px';
+levelDepth.style.width = '450px';
+
+pin.addEventListener('mousedown', function(evt) {
+	evt.preventDefault();
+	
+	var startCoords = {
+		x: evt.clientX
+	};
+	
+	var onMouseMove = function(moveEvt) {
+		moveEvt.preventDefault();
+		
+		if (startCoords.x >= 400 && startCoords.x <= 850) {
+			var shift = {
+				x: startCoords.x - moveEvt.clientX,
+			};
+			
+			startCoords = {
+				x: moveEvt.clientX,
+			};
+
+			pin.style.left = (pin.offsetLeft - shift.x) + 'px';
+			levelDepth.style.width = (moveEvt.clientX - 400) + 'px';
+			
+			effectLevelInput.value = Math.round(100 * parseInt(levelDepth.style.width) / 450);
+			
+			if (imagePreview.classList.contains('effects__preview--chrome')) {
+				imagePreview.style.filter = 'grayscale(' + effectLevelInput.value / 100 + ')';
+			} else if (imagePreview.classList.contains('effects__preview--sepia')) {
+				imagePreview.style.filter = 'sepia(' + effectLevelInput.value / 100 + ')';
+			} else if (imagePreview.classList.contains('effects__preview--marvin')) {
+				imagePreview.style.filter = 'invert(' + effectLevelInput.value + '%)';
+			} else if (imagePreview.classList.contains('effects__preview--phobos')) {
+				imagePreview.style.filter = 'blur(' + effectLevelInput.value / 100 * 3 + 'px)';
+			} else if (imagePreview.classList.contains('effects__preview--heat')) {
+				imagePreview.style.filter = 'brightness(' + (1 + effectLevelInput.value / 100 * 2) + ')';
+			}
+		}
+	};
+	
+	var onMouseUp = function(upEvt) {
+		upEvt.preventDefault();
+		
+		if (startCoords.x < 400) {
+			startCoords.x = 400;		
+			pin.style.left = '0px';
+			levelDepth.style.width = '0px';
+		}
+		
+		if (startCoords.x > 850) {
+			startCoords.x = 850;		
+			pin.style.left = '450px';
+			levelDepth.style.width = '450px';
+		}
+		
+		document.removeEventListener('mousemove', onMouseMove);
+		document.removeEventListener('mouseup', onMouseUp);
+	};
+	
+	document.addEventListener('mousemove', onMouseMove);
+	document.addEventListener('mouseup', onMouseUp);
 });
 
 
